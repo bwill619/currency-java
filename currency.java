cc9228c408f0844ef1355cc92492a5744f2f8bf7
Brendon Williams <bjw0619@gmail.com>
2021-06-07 23:50:26 -0400

    Create currency.java

---
+++ currency.java
@@ -0,0 +1,8 @@
+
+public class currency extends window {
+    public static void main (String [] args) {
+        window result = new window();
+        result.pack();
+        result.setVisible(true);
+    }
+}
