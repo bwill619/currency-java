a08b72d917ec4ebbc5114fe4b59f6a4f5f268722
Brendon Williams <bjw0619@gmail.com>
2021-06-08 00:47:09 -0400

    commit on 'main'

--- window.java
+++ window.java
@@ -13,9 +13,9 @@ public class window extends JFrame {
     private JLabel usd;
     private JLabel eur;
     private JButton convert;
+    private JButton clear;
     private JTextField input1;
     private JTextField input2;
-
 
     public window() {
         JFrame frame = new JFrame("Currency Converter");
@@ -23,5 +23,61 @@ public class window extends JFrame {
         setTitle("Currency Converter");
         setLayout(new GridBagLayout());
 
+        GridBagConstraints left1 = new GridBagConstraints();
+        left1.gridx = 0;
+        left1.gridy = 0;
+        left1.insets = new Insets(10, 10, 10, 10);
+        usd = new JLabel("USD");
+
+        GridBagConstraints left2 = new GridBagConstraints();
+        left2.gridx = 0;
+        left2.gridy = 1;
+        input1 = new JTextField(10);
+
+        GridBagConstraints middle1 = new GridBagConstraints();
+        middle1.gridx = 1;
+        middle1.gridy = 1;
+        convert = new JButton("Convert");
+
+        convert.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    double d1 = Double.parseDouble(input1.getText());
+
+                    double sum = ((.82) / d1);
+                    String st = String.valueOf(sum);
+                    input2.setText(st);
+
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        GridBagConstraints middle2 = new GridBagConstraints();
+        middle2.gridx = 1;
+        middle2.gridy = 2;
+        clear = new JButton("Clear");
+
+        GridBagConstraints right1 = new GridBagConstraints();
+        right1.gridx = 2;
+        right1.gridy = 0;
+        eur = new JLabel("EUR");
+
+        GridBagConstraints right2 = new GridBagConstraints();
+        right2.gridx = 2;
+        right2.gridy = 1;
+        input2 = new JTextField(10);
+
+        add(usd, left1);
+        add(input1, left2);
+        add(convert, middle1);
+        add(clear, middle2);
+        add(eur, right1);
+        add(input2, right2);
+
     }
+
 }
